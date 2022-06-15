6a2cefd0b495df88a1c6b5be6b7571e9c72cffa9
vparizot <107150059+vparizot@users.noreply.github.com>
2022-06-15 15:04:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace this_will_work_too
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rep = args;
+        }
+        static int sum(int num)
+        {
+            int prrr = num + 4;
+            return prrr;
+        }
+    }
+}
+
